d6d2e61a6e1b77c66c66f73949bf9dabb2156f88
ganjianping <ganjianping1984@gmail.com>
2026-01-21 22:08:56 +0800

    Fix navigation bar to be positioned at bottom of modal

--- src/pages/vocabularies/VocabularyDetail.tsx
+++ src/pages/vocabularies/VocabularyDetail.tsx
@@ -383,40 +383,40 @@ export default function VocabularyDetail({ vocabulary, onClose, onPrevious, onNe
                 </a>
               )}
             </div>
-            
-            {/* Navigation Buttons */}
-            {(hasPrevious || hasNext) && (
-              <div className="detail-navigation">
-                <button 
-                  className="detail-nav-btn"
-                  onClick={onPrevious}
-                  disabled={!hasPrevious}
-                  title={t('previous', language)}
-                  type="button"
-                >
-                  <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M15 18l-6-6 6-6" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-                  </svg>
-                  {t('previous', language)}
-                </button>
-                <button 
-                  className="detail-nav-btn"
-                  onClick={onNext}
-                  disabled={!hasNext}
-                  title={t('next', language)}
-                  type="button"
-                >
-                  {t('next', language)}
-                  <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M9 18l6-6-6-6" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-                  </svg>
-                </button>
-              </div>
-            )}
           </div>
 
           
         </div>
+        
+        {/* Navigation Buttons - Fixed at bottom */}
+        {(hasPrevious || hasNext) && (
+          <div className="detail-navigation">
+            <button 
+              className="detail-nav-btn"
+              onClick={onPrevious}
+              disabled={!hasPrevious}
+              title={t('previous', language)}
+              type="button"
+            >
+              <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M15 18l-6-6 6-6" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+              </svg>
+              {t('previous', language)}
+            </button>
+            <button 
+              className="detail-nav-btn"
+              onClick={onNext}
+              disabled={!hasNext}
+              title={t('next', language)}
+              type="button"
+            >
+              {t('next', language)}
+              <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M9 18l6-6-6-6" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+              </svg>
+            </button>
+          </div>
+        )}
       </div>
     </div>
   )
